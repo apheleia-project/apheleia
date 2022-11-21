fbce17fd913257e8715208a22465a4b237809135
Stuart Douglas <stuart.w.douglas@gmail.com>
2022-11-21 15:39:08 +1100

    Fix to allow republishing

--- deployment-tools/src/main/java/io/apheleia/DeployCommand.java
+++ deployment-tools/src/main/java/io/apheleia/DeployCommand.java
@@ -3,8 +3,7 @@ package io.apheleia;
 import com.amazonaws.services.codeartifact.AWSCodeArtifactClientBuilder;
 import com.amazonaws.services.codeartifact.model.DeletePackageVersionsRequest;
 import com.amazonaws.services.codeartifact.model.PackageFormat;
-import com.amazonaws.services.codeartifact.model.PackageVersionStatus;
-import com.amazonaws.services.codeartifact.model.UpdatePackageVersionsStatusRequest;
+import com.amazonaws.services.codeartifact.model.ResourceNotFoundException;
 import com.redhat.hacbs.classfile.tracker.ClassFileTracker;
 import com.redhat.hacbs.classfile.tracker.TrackingData;
 import io.apheleia.kube.RebuiltArtifact;
@@ -13,6 +12,7 @@ import io.fabric8.kubernetes.api.model.KubernetesResourceList;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
+import io.quarkus.logging.Log;
 import io.quarkus.picocli.runtime.annotations.TopCommand;
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
 import org.eclipse.aether.DefaultRepositorySystemSession;
@@ -48,6 +48,7 @@ import java.util.Optional;
 import java.util.function.UnaryOperator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @TopCommand
 @CommandLine.Command
@@ -89,118 +90,119 @@ public class DeployCommand implements Runnable {
             }
             for (var e : rebuiltArtifactMap.entrySet()) {
 
-                boolean deploy = false;
-                boolean delete = false;
-                for (var i : e.getValue()) {
-                    if (i.getMetadata().getAnnotations() != null) {
-                        String version = i.getMetadata().getAnnotations().get(APHELIA_DEPLOYED);
-                        if (version == null || Integer.parseInt(version) < CURRENT_VERSION) {
-                            if (version != null) {
-                                //need to remove existing
-                                delete = true;
+                try {
+                    boolean deploy = false;
+                    for (var i : e.getValue()) {
+                        if (i.getMetadata().getAnnotations() != null) {
+                            String version = i.getMetadata().getAnnotations().get(APHELIA_DEPLOYED);
+                            if (version == null || Integer.parseInt(version) < CURRENT_VERSION) {
+                                deploy = true;
+                                break;
                             }
+                        } else {
                             deploy = true;
                             break;
                         }
-                    } else {
-                        deploy = true;
-                        break;
                     }
-                }
-                if (!deploy) {
-                    continue;
-                }
-                var awsClient = AWSCodeArtifactClientBuilder.defaultClient();
+                    if (!deploy) {
+                        continue;
+                    }
+                    var awsClient = AWSCodeArtifactClientBuilder.defaultClient();
 
-                String image = e.getKey();
-                var finalDelete = delete;
-                Optional<Path> result = registryRepositoryClient.extractImage(image.substring(image.lastIndexOf(":") + 1));
-                if (result.isPresent()) {
-                    try {
-                        Files.walkFileTree(result.get(), new SimpleFileVisitor<>() {
+                    String image = e.getKey();
+                    Optional<Path> result = registryRepositoryClient.extractImage(image.substring(image.lastIndexOf(":") + 1));
+                    if (result.isPresent()) {
+                        try {
+                            Files.walkFileTree(result.get(), new SimpleFileVisitor<>() {
 
-                            @Override
-                            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                                try (var stream = Files.list(dir)) {
-                                    List<Path> files = stream.toList();
-                                    boolean hasPom = files.stream().anyMatch(s -> s.toString().endsWith(".pom"));
-                                    if (hasPom) {
+                                @Override
+                                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                                    try (var stream = Files.list(dir)) {
+                                        List<Path> files = stream.toList();
+                                        boolean hasPom = files.stream().anyMatch(s -> s.toString().endsWith(".pom"));
+                                        if (hasPom) {
 
-                                        Path relative = result.get().relativize(dir);
-                                        String group = relative.getParent().getParent().toString().replace("/", ".");
-                                        String artifact = relative.getParent().getFileName().toString();
-                                        String version = dir.getFileName().toString();
-                                        System.out.println("GROUP: " + group + " , ART:" + artifact + " , VERSION: " + version);
-                                        Pattern p = Pattern.compile(artifact + "-" + version + "(-(\\w+))?\\.(\\w+)");
+                                            Path relative = result.get().relativize(dir);
+                                            String group = relative.getParent().getParent().toString().replace("/", ".");
+                                            String artifact = relative.getParent().getFileName().toString();
+                                            String version = dir.getFileName().toString();
+                                            System.out.println("GROUP: " + group + " , ART:" + artifact + " , VERSION: " + version);
+                                            Pattern p = Pattern.compile(artifact + "-" + version + "(-(\\w+))?\\.(\\w+)");
 
-                                        if (finalDelete) {
-                                            DeletePackageVersionsRequest request = new DeletePackageVersionsRequest().withPackage(artifact)
-                                                    .withRepository("sdouglas-scratch")
-                                                    .withDomain("rhosak")
-                                                    .withFormat(PackageFormat.Maven)
-                                                    .withNamespace(group)
-                                                    .withVersions(version);
-                                            awsClient.deletePackageVersions(request);
-                                        }
-                                        DeployRequest deployRequest = new DeployRequest();
-                                        deployRequest.setRepository(distRepo);
-                                        for (var i : files) {
-                                            Matcher matcher = p.matcher(i.getFileName().toString());
-                                            if (matcher.matches()) {
-                                                System.out.println(i.getFileName());
-                                                System.out.println(matcher.group(2));
-                                                System.out.println(matcher.group(3));
-                                                if (matcher.group(3).equals("jar")) {
-                                                    var in = Files.readAllBytes(i);
-                                                    var out = ClassFileTracker.addTrackingDataToJar(in,
-                                                            new TrackingData(group + ":" + artifact + ":" + version, "rebuilt",
-                                                                    Collections.emptyMap()));
-                                                    Files.write(i, out);
-                                                    Files.writeString(
-                                                            i.getParent().resolve(i.getFileName().toString() + ".md5"),
-                                                            HashUtil.md5(out));
-                                                    Files.writeString(
-                                                            i.getParent().resolve(i.getFileName().toString() + ".sha1"),
-                                                            HashUtil.sha1(out));
+                                            try {
+                                                DeletePackageVersionsRequest request = new DeletePackageVersionsRequest().withPackage(artifact)
+                                                        .withRepository("sdouglas-scratch")
+                                                        .withDomain("rhosak")
+                                                        .withFormat(PackageFormat.Maven)
+                                                        .withNamespace(group)
+                                                        .withVersions(version);
+                                                awsClient.deletePackageVersions(request);
+                                            } catch (ResourceNotFoundException e) {
+                                                //not found
+                                            }
+                                            DeployRequest deployRequest = new DeployRequest();
+                                            deployRequest.setRepository(distRepo);
+                                            for (var i : files) {
+                                                Matcher matcher = p.matcher(i.getFileName().toString());
+                                                if (matcher.matches()) {
+                                                    System.out.println(i.getFileName());
+                                                    System.out.println(matcher.group(2));
+                                                    System.out.println(matcher.group(3));
+                                                    if (matcher.group(3).equals("jar")) {
+                                                        var in = Files.readAllBytes(i);
+                                                        var out = ClassFileTracker.addTrackingDataToJar(in,
+                                                                new TrackingData(group + ":" + artifact + ":" + version, "rebuilt",
+                                                                        Collections.emptyMap()));
+                                                        Files.write(i, out);
+                                                        Files.writeString(
+                                                                i.getParent().resolve(i.getFileName().toString() + ".md5"),
+                                                                HashUtil.md5(out));
+                                                        Files.writeString(
+                                                                i.getParent().resolve(i.getFileName().toString() + ".sha1"),
+                                                                HashUtil.sha1(out));
+                                                    }
+                                                    Artifact jarArtifact = new DefaultArtifact(group, artifact, matcher.group(2),
+                                                            matcher.group(3),
+                                                            version);
+                                                    jarArtifact = jarArtifact.setFile(i.toFile());
+                                                    deployRequest.addArtifact(jarArtifact);
                                                 }
-                                                Artifact jarArtifact = new DefaultArtifact(group, artifact, matcher.group(2),
-                                                        matcher.group(3),
-                                                        version);
-                                                jarArtifact = jarArtifact.setFile(i.toFile());
-                                                deployRequest.addArtifact(jarArtifact);
+                                            }
+
+                                            try {
+                                                system.deploy(session, deployRequest);
+                                            } catch (DeploymentException e) {
+                                                throw new RuntimeException(e);
                                             }
                                         }
 
-                                        try {
-                                            system.deploy(session, deployRequest);
-                                        } catch (DeploymentException e) {
-                                            throw new RuntimeException(e);
-                                        }
+                                        return FileVisitResult.CONTINUE;
                                     }
-
-                                    return FileVisitResult.CONTINUE;
                                 }
-                            }
 
-                        });
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                } else {
-                    System.err.println("Failed to download " + e.getKey());
-                }
-                for (var i : e.getValue()) {
-                    rebuildArtifacts.withName(i.getMetadata().getName()).edit(new UnaryOperator<RebuiltArtifact>() {
-                        @Override
-                        public RebuiltArtifact apply(RebuiltArtifact rebuiltArtifact) {
-                            if (rebuiltArtifact.getMetadata().getAnnotations() == null) {
-                                rebuiltArtifact.getMetadata().setAnnotations(new HashMap<>());
-                            }
-                            rebuiltArtifact.getMetadata().getAnnotations().put(APHELIA_DEPLOYED,
-                                    Integer.toString(CURRENT_VERSION));
-                            return rebuiltArtifact;
+                            });
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
                         }
-                    });
+                    } else {
+                        System.err.println("Failed to download " + e.getKey());
+                    }
+                    for (var i : e.getValue()) {
+                        rebuildArtifacts.withName(i.getMetadata().getName()).edit(new UnaryOperator<RebuiltArtifact>() {
+                            @Override
+                            public RebuiltArtifact apply(RebuiltArtifact rebuiltArtifact) {
+                                if (rebuiltArtifact.getMetadata().getAnnotations() == null) {
+                                    rebuiltArtifact.getMetadata().setAnnotations(new HashMap<>());
+                                }
+                                rebuiltArtifact.getMetadata().getAnnotations().put(APHELIA_DEPLOYED,
+                                        Integer.toString(CURRENT_VERSION));
+                                return rebuiltArtifact;
+                            }
+                        });
+                    }
+                } catch (Exception ex) {
+                    Log.errorf(ex, "Failed to handle image for %s", e.getValue().stream().map(s -> s.getSpec().getGav()).collect(Collectors.toList()));
+                    throw new RuntimeException(ex);
                 }
             }
         } catch (IOException e) {
